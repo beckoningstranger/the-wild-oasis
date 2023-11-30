11170aee0ced00b108bc6fecc98771a7eacb6f7a
beckoningstranger <jan.eisen@tutanota.de>
2023-11-30 15:06:30 +0100

    feat: Basic App Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "./ui/Sidebar";
+import Header from "./ui/Header";
+import styled from "styled-components";
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+const StyledApp = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
 
 export default function App() {
   return (
-    <>
-      <p>APP LAYOUT</p>
-      <Outlet />
-    </>
+    <StyledApp>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledApp>
   );
 }
