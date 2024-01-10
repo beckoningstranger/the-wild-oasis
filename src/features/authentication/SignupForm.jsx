bda80e37cb64280f8be742fa556dbdf771f41bd0
beckoningstranger <jan.eisen@tutanota.de>
2024-01-10 15:21:18 +0100

    feat: Implement Signup

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,15 +3,18 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignup } from "./useSignup";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
+  const { signup, isLoading } = useSignup();
 
-  function onSubmit(data) {
-    console.log(data);
+  function onSubmit({ fullName, password, email }) {
+    signup({ fullName, email, password }, { onSettled: () => reset() });
   }
 
   return (
@@ -20,6 +23,7 @@ function SignupForm() {
         <Input
           type="text"
           id="fullName"
+          disabled={isLoading}
           {...register("fullName", { required: "This is field is required" })}
         />
       </FormRow>
@@ -28,6 +32,7 @@ function SignupForm() {
         <Input
           type="email"
           id="email"
+          disabled={isLoading}
           {...register("email", {
             required: "This is field is required",
             pattern: {
@@ -45,6 +50,7 @@ function SignupForm() {
         <Input
           type="password"
           id="password"
+          disabled={isLoading}
           {...register("password", {
             required: "This is field is required",
             minLength: {
@@ -59,6 +65,7 @@ function SignupForm() {
         <Input
           type="password"
           id="passwordConfirm"
+          disabled={isLoading}
           {...register("passwordConfirm", {
             required: "This is field is required",
             validate: (value) =>
@@ -69,10 +76,12 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button $variation="secondary" type="reset">
+        <Button $variation="secondary" type="reset" disabled={isLoading}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>
+          {isLoading ? <SpinnerMini /> : "Create new user"}
+        </Button>
       </FormRow>
     </Form>
   );
