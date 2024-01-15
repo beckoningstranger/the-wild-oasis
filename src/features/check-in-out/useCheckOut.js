04422588ffb1aab06286504eff0f17dfe47d546d
beckoningstranger <jan.eisen@tutanota.de>
2024-01-15 15:28:02 +0100

    feat: Check guests in and out from dashboard

--- src/features/check-in-out/useCheckOut.js
+++ src/features/check-in-out/useCheckOut.js
@@ -2,10 +2,10 @@ import { useNavigate } from "react-router-dom";
 import { updateBooking } from "../../services/apiBookings";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { useEffect, useState } from "react";
 
 export function useCheckout() {
   const queryClient = useQueryClient();
-  const navigate = useNavigate();
 
   const { mutate: checkout, isLoading: isCheckingOut } = useMutation({
     mutationFn: (bookingId) =>
@@ -16,8 +16,8 @@ export function useCheckout() {
     onSuccess: (data) => {
       toast.success(`Booking #${data.id} successfully checked out`);
       queryClient.invalidateQueries({ active: true });
-      navigate("/");
     },
+
     onError: () => {
       toast.error("There was an error checking out.");
     },
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import { SalesChart } from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -16,12 +17,7 @@ const StyledDashboardLayout = styled.div`
 
 export default function DashboardLayout() {
   const { bookings, isLoading: loadingBookings } = useRecentBookings();
-  const {
-    stays,
-    confirmedStays,
-    isLoading: loadingStays,
-    numDays,
-  } = useRecentStays();
+  const { confirmedStays, isLoading: loadingStays, numDays } = useRecentStays();
   const { cabins, isLoading: loadingCabins } = useCabins();
 
   if (loadingBookings || loadingStays || loadingCabins) return <Spinner />;
@@ -34,7 +30,7 @@ export default function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today&apos;s activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
